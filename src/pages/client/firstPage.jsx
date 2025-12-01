1e823e2bd3978bd4adc4a5f8165ea070f7cc11c4
Malshan <23app6394@ms.sab.ac.lk>
2025-12-01 11:00:15 +0530

    add some comments 🤒🤒

--- src/pages/client/firstPage.jsx
+++ src/pages/client/firstPage.jsx
@@ -4,7 +4,9 @@ export default function FirstPage() {
 
 return(
     <div className="first-page">
+    
         <div className="welcome"> 
+            {/* welcome to pet care */}
             <h1>Welcome to Pet Care</h1>
             <p>Your one-stop shop for all your pet needs!</p>
         </div>
